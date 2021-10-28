fe72c707d9ecabdf791cfd2b97b04d7dd04878c8
arturolegovich <82318393+arturolegovich@users.noreply.github.com>
2021-10-28 21:04:35 +0400

    Add files via upload

--- phpviewer_clientsdb/index.php
+++ phpviewer_clientsdb/index.php
@@ -1,53 +1,59 @@
+<!DOCTYPE html>
+
+<html lang=ru>
+<head>
+<meta charset=utf-8>
+<title>py-kms server</title>
+<meta name=description content="py-kms server">
+<meta name=keywords content=php py-kms>
+<body>
+
 <?php
 # Change variables for youself!
 $sqlite_path = "/opt/share/py-kms";
-
 #######################
 # ---- START SCRIPT----
 #######################
 $dbn = "sqlite:$sqlite_path/clients.db";
-
 try {
 $dbh = new PDO($dbn);
 } catch (PDOException $e) {
 echo $e->getMessage();
 }
-
 $sql = "SELECT sum(requestCount) FROM clients";
 foreach ($dbh->query($sql) as $row) {
 	$requestCount = $row[0];
 }
-
 $sql = "SELECT clientMachineId, machineName, applicationId, skuId, licenseStatus, lastRequestTime, kmsEpid, requestCount FROM clients";
-
-echo "<table border=1 cellspacing=0 cellpadding=2 width=95% align=center>";
-echo "<thead bgcolor=silver>";
-echo "<tr>";
-echo "<th>Machine name</th>";
-echo "<th>App ID</th>";
-echo "<th>App version</th>";
-echo "<th>License status</th>";
-echo "<th>Last request time</th>";
-echo "<th>Request count</th>";
-echo "</tr>";
-echo "<thead>";
+?>
+<table border=1 cellspacing=0 cellpadding=2 width=95% align=center>
+	<thead bgcolor=silver>
+	<tr>
+		<th>Machine name</th><th>App ID</th><th>App version</th><th>License status</th><th>Last request time</th><th>Request count</th>
+	</tr>
+	<thead>
+<?php
 foreach ($dbh->query($sql) as $row) {
-    echo "<tr>";
-	  //echo "<td>" . $row['clientMachineId'] . "</td>";
-      echo "<td>" . $row['machineName'] . "</td>";
-	  echo "<td>" . $row['applicationId'] . "</td>";
-      echo "<td>" . $row['skuId'] . "</td>";
-	  echo "<td>" . $row['licenseStatus'] . "</td>";
-      echo "<td>" . date("d.m.Y H:i:s",$row['lastRequestTime']) . "</td>";
-	  //echo "<td>" . $row['kmsEpid'] . "</td>";
-      echo "<td>" . $row['requestCount'] . "</td>";
-	echo "</tr>";
+	echo "	<tr>
+		<td>" . $row['machineName'] . "</td>
+		<td>" . $row['applicationId'] . "</td>
+		<td>" . $row['skuId'] . "</td>
+		<td>" . $row['licenseStatus'] . "</td>
+		<td>" . date('d.m.Y H:i:s',$row['lastRequestTime']). "</td>
+		<td>" .$row['requestCount'] . "</td>
+	</tr>\n";
 }
-	echo "<tr bgcolor=silver>";
-	  echo "<td colspan=5><b>Total request count:</b></td>";
-	  echo "<td>" . $requestCount . "</td>";
-	echo "</tr>";
-echo "</table>";
-
+?>
+	<tr bgcolor=silver>
+		<td colspan=5><b>Total request count:</b></td>
+<?php		
+		echo "	<td>$requestCount</td>\n";
+?>
+	</tr>
+</table>
+<?php
 $dbh = NULL;
 ?>
+
+</body>
+</html>
